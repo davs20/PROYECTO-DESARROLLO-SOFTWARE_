0df7fd21ee75ac869d68f79feede67400fa4ccdc
AdonayCalix <adonaymorazan@gmail.com>
2018-03-31 20:59:06 -0600

    Add

--- layouts/footer.php
+++
@@ -1,5 +0,0 @@
-<!-- Main Footer -->
-<footer class="main-footer">
-    <strong>Copyright &copy; 2018-<?php echo date('Y'); ?>
-    <a target="_blank" href="#">Desarrollo de Software</a>.
-</footer>
---
+++ modelo/Conexion.php
@@ -0,0 +1,35 @@
+<?php
+
+class Conexion
+{
+    protected $configuracion = [
+        'driver' => 'mysql',
+        'host' => '127.0.0.1',
+        'nombreDB' => 'sistema_hotel',
+        'usuario' => 'root',
+        'contreaseña' => 'K17OACX' // Aqui va su contraseña, preferiblemente que todos usemos la misma.
+    ];
+
+    protected $conexion;
+
+    public function obtenerConexion()
+    {
+        if ($this->conexion == null) {
+            $server = "" .
+                $this->configuracion['driver'] .
+                ":host=" . $this->configuracion['host'] .
+                ";dbname=" . $this->configuracion['nombreDB'];
+
+            try {
+                $this->conexion = new PDO($server, $this->configuracion['usuario'], $this->configuracion['contreaseña']);
+            } catch (PDOException $exception) {
+                echo "Upps algo malo paso con la conexion " . $exception->getMessage();
+            }
+        }
+    }
+
+    public function destruirConexion()
+    {
+        $this->conexion = null;
+    }
+}
