a5d821ba4eb955b0b086d561d6153f9346e87604
AdonayCalix <adonaymorazan@gmail.com>
2018-03-31 20:49:17 -0600

    Agrege los archivos de vistas

---
+++ vistas/layouts/footer.php
@@ -0,0 +1,5 @@
+<!-- Main Footer -->
+<footer class="main-footer">
+    <strong>Copyright &copy; 2018-<?php echo date('Y'); ?>
+    <a target="_blank" href="#">Desarrollo de Software</a>.
+</footer>
---
+++ vistas/layouts/sidebar_administrador.php
@@ -0,0 +1,25 @@
+<!-- Left side column. contains the logo and sidebar -->
+<aside class="main-sidebar">
+  <section class="sidebar">
+    <ul class="sidebar-menu">
+      <li class="header">Menu Principal</li>
+      <li class="active"><a href="#"><i class='fa fa-home'></i> <span>Inicio</span></a></li>
+      <li class="treeview">
+        <a href="#"><i class='fa fa-area-chart'></i> <span>Estadisticas</span> </a>
+      </li>
+      <li class="treeview">
+          <a href="#"><i class='fa fa-building-o'></i> <span>Gestión de Habitaciones</span> </i></a>
+      </li>
+      <li class="treeview">
+          <a href="#"><i class='fa fa-users'></i> <span>Gestión de Usuarios</span> </i></a>
+      </li>
+      <li class="treeview">
+          <a href="#"><i class='fa fa-wrench'></i> <span>Configuraciones</span> <i class="fa fa-angle-left pull-right"></i></a>
+            <ul class="treeview-menu">
+                <li><a href="#">Precio de Habitaciones</a></li>
+                <li><a href="#">Soporte tecnico</a></li>
+            </ul>
+      </li>
+    </ul>
+  </section>
+</aside>
